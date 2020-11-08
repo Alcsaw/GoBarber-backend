d1518e2af964bee17a1cbf10bf21e292a665d70c
Augusto Schnorr <alcsaw@hotmail.com>
2020-11-08 10:55:56 -0300

    Converts model to Entity & AppointmentsRepository to Entityrepository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,28 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmenstRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmenstRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmenstRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmenstRouter.post('/', (request, response) => {
+appointmenstRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({
+    const appointment = await createAppointment.execute({
       provider,
       date: parsedDate,
     });
